d8f51cacd768bd02ffe1a0160c84ddcbdb99fb67
Tinoskk03Tech <kossivitinek@gmail.com>
2025-08-08 21:17:16 +0000

    Je viens de finir l'exercice du jour 17

---
+++ Day_17/exception_handling.py
@@ -0,0 +1,12 @@
+# 1. Unpacking des pays nordiques
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+# Décomposition
+nordic_countries = names[:5]
+es = names[5]
+ru = names[6]
+
+# Affichage des résultats
+print("Nordic countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
